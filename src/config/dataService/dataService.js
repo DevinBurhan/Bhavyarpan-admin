755d222fc763d248c8c2d0b9dbebe8377129bfaa
parth.devstree <parth.devstree@gmail.com>
2024-07-21 15:17:41 +0530

    errors sovled

--- src/config/dataService/dataService.js
+++ src/config/dataService/dataService.js
@@ -4,64 +4,64 @@ import { getItem } from "../../utility/localStorageControl";
 const API_ENDPOINT = "https://api.bhavyarpan.com/";
 
 const authHeader = () => ({
-  Authorization: `Bearer ${getItem("access_token")}`,
+    Authorization: `Bearer ${getItem("access_token")}`,
 });
 
 const client = axios.create({
-  baseURL: API_ENDPOINT,
-  headers: {
-    Authorization: `Bearer ${getItem("access_token")}`,
-    // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    // "Content-Type": "application/json",
-  },
+    baseURL: API_ENDPOINT,
+    headers: {
+        Authorization: `Bearer ${getItem("access_token")}`,
+        // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        // "Content-Type": "application/json",
+    },
 });
 
 class DataService {
-  static get(path = "", params = {}) {
-    return client({
-      method: "GET",
-      url: path,
-      headers: { ...authHeader() },
-      params: {
-        ...params, // Include any additional params passed
-      },
-    });
-  }
+    static get(path = "", params = {}) {
+        return client({
+            method: "GET",
+            url: path,
+            headers: { ...authHeader() },
+            params: {
+                ...params, // Include any additional params passed
+            },
+        });
+    }
 
-  static post(path = "", data = {}, optionalHeader = {}) {
-    return client({
-      method: "POST",
-      url: path,
-      data,
-      headers: { ...optionalHeader, ...authHeader() },
-    });
-  }
+    static post(path = "", data = {}, optionalHeader = {}) {
+        return client({
+            method: "POST",
+            url: path,
+            data,
+            headers: { ...optionalHeader, ...authHeader() },
+        });
+    }
 
-  static patch(path = "", data = {}) {
-    return client({
-      method: "PATCH",
-      url: path,
-      data,
-      headers: { ...authHeader() },
-    });
-  }
+    static patch(path = "", data = {}) {
+        return client({
+            method: "PATCH",
+            url: path,
+            data,
+            headers: { ...authHeader() },
+        });
+    }
 
-  static put(path = "", data = {}) {
-    return client({
-      method: "PUT",
-      url: path,
-      data,
-      headers: { ...authHeader() },
-    });
-  }
-  static delete(path = "", data = {}) {
-    return client({
-      method: "DELETE",
-      url: path,
-      data,
-      headers: { ...authHeader() },
-    });
-  }
+    static put(path = "", data = {}) {
+        return client({
+            method: "PUT",
+            url: path,
+            data,
+            headers: { ...authHeader() },
+        });
+    }
+    static delete(path = "", data = {}) {
+        return client({
+            method: "DELETE",
+            url: path,
+            data,
+            headers: { ...authHeader() },
+        });
+    }
 }
 
 /**
@@ -69,35 +69,35 @@ class DataService {
  * For more details on axios interceptor see https://github.com/axios/axios#interceptors
  */
 client.interceptors.request.use((config) => {
-  // do something before executing the request
-  // For example tag along the bearer access token to request header or set a cookie
-  const requestConfig = config;
-  const { headers } = config;
-  requestConfig.headers = {
-    ...headers,
-    Authorization: `Bearer ${getItem("access_token")}`,
-  };
+    // do something before executing the request
+    // For example tag along the bearer access token to request header or set a cookie
+    const requestConfig = config;
+    const { headers } = config;
+    requestConfig.headers = {
+        ...headers,
+        Authorization: `Bearer ${getItem("access_token")}`,
+    };
 
-  return requestConfig;
+    return requestConfig;
 });
 
 client.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    /**
-     * Do something in case the response returns an error code [3**, 4**, 5**] etc
-     * For example, on token expiration retrieve a new access token, retry a failed request etc
-     */
-    const { response } = error;
-    const originalRequest = error.config;
-    if (response) {
-      if (response.status === 500) {
-        // do something here
-      } else {
-        return originalRequest;
-      }
+    (response) => response,
+    (error) => {
+        /**
+         * Do something in case the response returns an error code [3**, 4**, 5**] etc
+         * For example, on token expiration retrieve a new access token, retry a failed request etc
+         */
+        const { response } = error;
+        // const originalRequest = error.config;
+        if (response) {
+            if (response.status === 500) {
+                // do something here
+            } else {
+                return response;
+            }
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 export { DataService };
--- src/container/category/subcategory.js
+++ src/container/category/subcategory.js
@@ -1,256 +1,202 @@
 import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
-import {
-  Button,
-  Col,
-  Form,
-  Input,
-  Modal,
-  Row,
-  Select,
-  Space,
-  Table,
-} from "antd";
+import { Button, Col, Form, Input, Modal, Row, Select, Space, Table } from "antd";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategoiryAPI } from "../../redux/categoryredux/actionCreator";
-import {
-  addSubcategoryAPI,
-  deleteSubcategoryAPI,
-  getSubcategoriesAPI,
-  updateSubcategoryAPI,
-} from "../../redux/subcategoryredux/actionCreator";
+import { addSubcategoryAPI, deleteSubcategoryAPI, getSubcategoriesAPI, updateSubcategoryAPI } from "../../redux/subcategoryredux/actionCreator";
 
 const SubCategoryPage = () => {
-  const [form] = Form.useForm();
-  const dispatch = useDispatch();
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(10);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedsubCategory, setSelectedsubCategory] = useState(null);
-  const [isLoading, setIsLoading] = useState(false); //loadder
-  const [selectedId, setSelectedId] = useState(null);
+    const [form] = Form.useForm();
+    const dispatch = useDispatch();
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(10);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedsubCategory, setSelectedsubCategory] = useState(null);
+    const [isLoading, setIsLoading] = useState(false); //loadder
+    const [selectedId, setSelectedId] = useState(null);
 
-  const data = useSelector((state) => state?.subcategoryReducer?.subCategory);
-  const categoryList = useSelector(
-    (state) => state?.categoryReducer?.Categories
-  );
+    const data = useSelector((state) => state?.subcategoryReducer?.subCategory);
+    const categoryList = useSelector((state) => state?.categoryReducer?.Categories);
 
-  useEffect(() => {
-    getApi(true, page, limit);
-    getCategoryList();
-  }, []);
+    useEffect(() => {
+        getApi(true, page, limit);
+        getCategoryList();
+    }, []);
 
-  const getApi = async (pagination, page, limit) => {
-    setIsLoading(true);
-    let params = {
-      pagination,
-      page,
-      limit,
+    const getApi = async (pagination, page, limit) => {
+        setIsLoading(true);
+        let params = {
+            pagination,
+            page,
+            limit,
+        };
+        await dispatch(getSubcategoriesAPI(params));
+        setIsLoading(false);
     };
-    await dispatch(getSubcategoriesAPI(params));
-    setIsLoading(false);
-  };
-  const onPageChange = async (page) => {
-    setPage(page);
-    getApi(true, page, limit);
-  };
-  const getCategoryList = async () => {
-    setIsLoading(true);
-    let params = {
-      pagination: false,
+    const onPageChange = async (page) => {
+        setPage(page);
+        getApi(true, page, limit);
     };
-    await dispatch(getCategoiryAPI(params));
-    setIsLoading(false);
-  };
+    const getCategoryList = async () => {
+        setIsLoading(true);
+        let params = {
+            pagination: false,
+        };
+        await dispatch(getCategoiryAPI(params));
+        setIsLoading(false);
+    };
 
-  const handleEdit = (record) => {
-    setSelectedId(record?._id);
-    form.setFieldsValue(record); //set Antd Form Value on EDIT
-    setIsModalOpen(true);
-  };
+    const handleEdit = (record) => {
+        setSelectedId(record?._id);
+        form.setFieldsValue(record); //set Antd Form Value on EDIT
+        setIsModalOpen(true);
+    };
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    setSelectedId(null);
-    form.resetFields();
-  };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+        setSelectedId(null);
+        form.resetFields();
+    };
 
-  const handleDelete = (record) => {
-    Modal.confirm({
-      title: "Are you sure you want to delete this category",
-      onOk: async () => {
-        if (record._id) {
-          let resp = await dispatch(deleteSubcategoryAPI(record._id));
-          if (resp) {
-            getApi();
-          }
+    const handleDelete = (record) => {
+        Modal.confirm({
+            title: "Are you sure you want to delete this category",
+            onOk: async () => {
+                if (record._id) {
+                    let resp = await dispatch(deleteSubcategoryAPI(record._id));
+                    if (resp) {
+                        getApi(true, page, limit);
+                    }
+                }
+            },
+        });
+    };
+    const handleFinish = async (values) => {
+        setIsLoading(true);
+
+        let resp;
+
+        if (selectedId !== null) {
+            //update logic
+
+            resp = await dispatch(updateSubcategoryAPI(selectedId, values)); //update api
+        } else {
+            //add logic
+
+            resp = await dispatch(addSubcategoryAPI(values)); //add api
         }
-      },
-    });
-  };
-  const handleFinish = async (values) => {
-    setIsLoading(true);
+        // console.log("safdsfgf");
+        if (resp) {
+            handleCancel();
+            getApi(true, page, limit);
+        } else {
+            getApi(true, page, limit);
+        }
+    };
 
-    let resp;
+    const columns = [
+        {
+            title: "Sr. No",
+            dataIndex: "key",
+            key: "key",
+            render: (text, object, index) => index + 1 + (page - 1) * limit,
+        },
+        {
+            title: "Title",
+            dataIndex: "title",
+            key: "title",
+        },
+        {
+            title: "Description",
+            dataIndex: "description",
+            key: "description",
+        },
 
-    if (selectedId !== null) {
-      //update logic
+        {
+            title: "Action",
+            key: "action",
+            render: (text, record) => (
+                <Space size="middle">
+                    <Button type="" icon={<EditOutlined />} size="small" onClick={() => handleEdit(record)}>
+                        Edit
+                    </Button>
+                    <Button type="" icon={<DeleteOutlined />} size="small" onClick={() => handleDelete(record)}>
+                        Delete
+                    </Button>
+                </Space>
+            ),
+        },
+    ];
 
-      resp = await dispatch(updateSubcategoryAPI(selectedId, values)); //update api
-    } else {
-      //add logic
+    return (
+        <div className="pageHeading">
+            <Row gutter={20} justify={"space-between"}>
+                <Col>
+                    <h2>SubCategory</h2>
+                </Col>
+                <Col>
+                    <Row gutter={[20, 20]}>
+                        <Col>{/* <Input type="search" placeholder={"Search categroy"} style={{ marginBottom: "25px" }} prefix={<SearchOutlined />} /> */}</Col>
+                        <Col>
+                            <Button style={{ height: "47.56px" }} type="primary" icon={<PlusOutlined />} onClick={() => setIsModalOpen(true)}>
+                                Add Subcategory
+                            </Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
 
-      resp = await dispatch(addSubcategoryAPI(values)); //add api
-    }
-    // console.log("safdsfgf");
-    if (resp) {
-      handleCancel();
-      getApi(true, page, limit);
-    } else {
-      getApi(true, page, limit);
-    }
-  };
+            <Table
+                dataSource={data && data.data ? data.data : []}
+                columns={columns}
+                loading={isLoading}
+                pagination={{
+                    showSizeChanger: false,
+                    pageSize: limit,
+                    total: data?.totalCount,
+                    current: page,
+                    onChange: onPageChange,
+                }}
+            />
 
-  const columns = [
-    {
-      title: "Sr. No",
-      dataIndex: "key",
-      key: "key",
-      render: (text, object, index) => index + 1 + (page - 1) * limit,
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      key: "title",
-    },
-    {
-      title: "Description",
-      dataIndex: "description",
-      key: "description",
-    },
-
-    {
-      title: "Action",
-      key: "action",
-      render: (text, record) => (
-        <Space size="middle">
-          <Button
-            type=""
-            icon={<EditOutlined />}
-            size="small"
-            onClick={() => handleEdit(record)}
-          >
-            Edit
-          </Button>
-          <Button
-            type=""
-            icon={<DeleteOutlined />}
-            size="small"
-            onClick={() => handleDelete(record)}
-          >
-            Delete
-          </Button>
-        </Space>
-      ),
-    },
-  ];
-
-  return (
-    <div className="pageHeading">
-      <Row gutter={20} justify={"space-between"}>
-        <Col>
-          <h2>SubCategory</h2>
-        </Col>
-        <Col>
-          <Row gutter={[20, 20]}>
-            <Col>
-              {/* <Input type="search" placeholder={"Search categroy"} style={{ marginBottom: "25px" }} prefix={<SearchOutlined />} /> */}
-            </Col>
-            <Col>
-              <Button
-                style={{ height: "47.56px" }}
-                type="primary"
-                icon={<PlusOutlined />}
-                onClick={() => setIsModalOpen(true)}
-              >
-                Add Subcategory
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-
-      <Table
-        dataSource={data && data.data ? data.data : []}
-        columns={columns}
-        loading={isLoading}
-        pagination={{
-          showSizeChanger: false,
-          pageSize: limit,
-          total: data?.totalCount,
-          current: page,
-          onChange: onPageChange,
-        }}
-      />
-
-      <Modal
-        title={selectedsubCategory ? "Edit subCategory" : "Add subCategory"}
-        open={isModalOpen}
-        onCancel={handleCancel}
-        onOk={form.submit}
-        destroyOnClose
-        okText="Submit"
-        cancelText="Cancel"
-        confirmLoading={isLoading}
-      >
-        <Form form={form} layout="vertical" onFinish={handleFinish}>
-          <Form.Item
-            label="Title"
-            name="title"
-            rules={[{ required: true, message: "Please input the title!" }]}
-          >
-            <Input placeholder="Enter title" />
-          </Form.Item>
-
-          <Form.Item
-            label="Description"
-            name="description"
-            rules={[
-              { required: true, message: "Please input the description!" },
-            ]}
-          >
-            <Input.TextArea placeholder="Enter description" />
-          </Form.Item>
-          <Form.Item
-            label="Select Category"
-            name="category"
-            rules={[
-              { required: true, message: "Please input the description!" },
-            ]}
-          >
-            <Select
-              size="large"
-              showSearch
-              placeholder="Select Category"
-              filterOption={(input, option) =>
-                (option?.label ?? "")
-                  .toLowerCase()
-                  .includes(input.toLowerCase())
-              }
+            <Modal
+                title={selectedsubCategory ? "Edit subCategory" : "Add subCategory"}
+                open={isModalOpen}
+                onCancel={handleCancel}
+                onOk={form.submit}
+                destroyOnClose
+                okText="Submit"
+                cancelText="Cancel"
+                confirmLoading={isLoading}
             >
-              {categoryList?.data?.length > 0 &&
-                categoryList.data.map((item, ind) => (
-                  <Select.Option key={ind} value={item._id}>
-                    {console.log("item", item)}
-                    {item?.title}
-                  </Select.Option>
-                ))}
-            </Select>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </div>
-  );
+                <Form form={form} layout="vertical" onFinish={handleFinish}>
+                    <Form.Item label="Title" name="title" rules={[{ required: true, message: "Please input the title!" }]}>
+                        <Input placeholder="Enter title" />
+                    </Form.Item>
+
+                    <Form.Item label="Description" name="description" rules={[{ required: true, message: "Please input the description!" }]}>
+                        <Input.TextArea placeholder="Enter description" />
+                    </Form.Item>
+                    <Form.Item label="Select Category" name="category" rules={[{ required: true, message: "Please input the description!" }]}>
+                        <Select
+                            size="large"
+                            showSearch
+                            placeholder="Select Category"
+                            filterOption={(input, option) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase())}
+                        >
+                            {categoryList?.data?.length > 0 &&
+                                categoryList.data.map((item, ind) => (
+                                    <Select.Option key={ind} value={item._id}>
+                                        {console.log("item", item)}
+                                        {item?.title}
+                                    </Select.Option>
+                                ))}
+                        </Select>
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </div>
+    );
 };
 
 export default SubCategoryPage;
